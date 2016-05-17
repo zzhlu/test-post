67d35101f6733eb0e8539d7af4a4f2efaef31f08
zhaolu <1632089314@qq.com>
2016-05-17 14:03:27 +0800

    finished

---
+++ src/collect_all_even.js
@@ -0,0 +1,20 @@
+function collect_same_elements(collection_a, object_b) {
+  var newArray = [];
+  collection_a.forEach(function (element) {
+    if (isExist(element.key, object_b.value)) {
+      newArray.push(element.key);
+    }
+  });
+  return  newArray;
+}
+
+function isExist(element, collection) {
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i] === element) {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = collect_same_elements;
